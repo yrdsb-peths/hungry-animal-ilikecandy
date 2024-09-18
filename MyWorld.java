4350ae8214023fea5e300d5510c2685d50eb55b9
Jacob Tian <39249179+ilikecandy@users.noreply.github.com>
2024-09-18 13:19:58 -0400

    add elephant and apple

--- MyWorld.java
+++ MyWorld.java
@@ -3,19 +3,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Jacob Tian
+ * @version September 2024
  */
 public class MyWorld extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * The main world for the hero.
      * 
      */
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        Apple apple = new Apple();
+        addObject(apple, 300, 0);
     }
 }

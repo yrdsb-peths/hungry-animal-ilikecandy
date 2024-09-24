8406ce9ee5786f4d3284584a17943b718a12f301
Jacob Tian <39249179+ilikecandy@users.noreply.github.com>
2024-09-24 09:54:45 -0400

    Add elephant eat sound

--- Elephant.java
+++ Elephant.java
@@ -8,10 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    
     public void act()
     {
         // Add your action code here.
@@ -35,6 +33,8 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
+            
+            elephantSound.play();
         }
     }
 }

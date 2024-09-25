ac25f8095bf470f6191dc88b7019d697312d561f
ilikecandy <jacobnaonao7@gmail.com>
2024-09-24 23:15:53 -0400

    Make title screen fancy

--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
+    Label titleLabel = new Label("The Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -17,10 +17,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, getWidth()/2 - 100, 100);
+        prepare();
     }
-    
+
     /**
      * Main world act loop
      */
@@ -30,6 +31,20 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
-        
+
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,464,106);
+        Label label = new Label("Use \u2190 and \u2192 arrows to move", 40);
+        addObject(label,245,209);
+        Label label2 = new Label("Press space to start!", 40);
+        addObject(label2,298,271);
     }
 }
